49bdd3cba2bd1431903542f09a2f3b7e578e7be0
etiotto <56368199+etiotto@users.noreply.github.com>
2019-10-23 14:31:24 -0700

    Update LoopOptTutorial.h

--- llvm/include/llvm/Transforms/Scalar/LoopOptTutorial.h
+++ llvm/include/llvm/Transforms/Scalar/LoopOptTutorial.h
@@ -17,9 +17,9 @@
 #define LLVM_TRANSFORMS_SCALAR_LOOPOPTTUTORIAL_H
 
 #include "llvm/Analysis/LoopAnalysisManager.h"
-#include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Transforms/Utils/ValueMapper.h"
+#include "llvm/Analysis/LoopInfo.h"
 
 namespace llvm {
 
@@ -57,6 +57,7 @@ private:
   Instruction *computeSplitPoint(const Loop &L,
                                  Instruction *InsertBefore) const;
 
+  
   /// Get the latch comparison instruction of loop \p L.
   ICmpInst *getLatchCmpInst(const Loop &L) const;
 
@@ -68,7 +69,7 @@ private:
   // Dump the LLVM IR for function \p F.
   void dumpFunction(const StringRef Msg, const Function &F) const;
 
-private:
+ private:
   LoopInfo &LI;
   ScalarEvolution &SE;
   DominatorTree &DT;
